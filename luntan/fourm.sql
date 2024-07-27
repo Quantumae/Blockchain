682d3fe57aa9fbfd1a2715eb5573edf4f5656c80
sjh20031214 <1152349857@qq.com>
2024-07-27 20:31:28 +0800

    小修改

--- luntan/fourm.sql
+++ luntan/fourm.sql
@@ -4,5 +4,6 @@ USE forum;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    public_key VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
